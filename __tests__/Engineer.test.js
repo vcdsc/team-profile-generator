789d97e2cbe4dcc54ae13c436e938118d06d79d8
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-03-05 10:24:36 +0000

    add Engineer class and corresponding properties and methods
    at the point of commit, Engineer class passes all Jest tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,12 @@
 const Engineer = require("../lib/Engineer");
 
-test("Can set GitHUb account via constructor", () => {
+test("Can set GitHub account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
-test("getRole() should return \"Engineer\"", () => {
+test('getRole() should return "Engineer"', () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
@@ -15,5 +15,5 @@ test("getRole() should return \"Engineer\"", () => {
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
+  expect(e.getGitHub()).toBe(testValue);
 });
